559c251bea3f0d6e8d3813eaa18bf21a36642ed0
Madhav10Mahajan <madhav.mhjn01@gmail.com>
2025-05-22 01:54:02 +0530

    iterator pattern

---
+++ LLD/BehaviouralDesignPattern/iteratorDesignPattern.java
@@ -0,0 +1,8 @@
+/* used for iterating a collecton of objects without exposing underlying implementation */
+package BehaviouralDesignPattern;
+
+public class iteratorDesignPattern {
+    public static void main(String[] args) {
+        
+    }
+}
--- LLD/BehaviouralDesignPattern/observerDesignPattern.java
+++ LLD/BehaviouralDesignPattern/observerDesignPattern.java
@@ -7,66 +7,117 @@
 package BehaviouralDesignPattern;
 
 import java.util.ArrayList;
+import java.util.List;
 
-class YoutubeChannel {
+// class YoutubeChannel {
 
-    public String channelName;
-    public String video;
-    public ArrayList<Subscriber> subscriberList = new ArrayList<>();
+//     public String channelName;
+//     public String video;
+//     public ArrayList<Subscriber> subscriberList = new ArrayList<>();
 
-    YoutubeChannel(String channelName) {
-        this.channelName = channelName;
+//     YoutubeChannel(String channelName) {
+//         this.channelName = channelName;
+//     }
+
+//     public void addSubscriber(Subscriber subscriber) {
+//         this.subscriberList.add(subscriber);
+//     }
+
+//     public void uploadVideo(String video) {
+//         this.video = video;
+//         notifySubscribers(video);
+//     }
+
+//     public void notifySubscribers(String video) {
+
+//         for (Subscriber subscriber : this.subscriberList) {
+//             System.out.println("hello " + subscriber.name + " a new video " + this.video + " has been uploaded");
+//         }
+//     }
+
+// }
+
+// class Subscriber {
+//     public String name;
+//     public String id;
+//     public String email;
+//     public ArrayList<YoutubeChannel> channels = new ArrayList<>();
+
+//     Subscriber(String name, String id, String email) {
+//         this.id = id;
+//         this.email = email;
+//         this.name = name;
+//     }
+
+//     public void addChannel(YoutubeChannel channel) {
+//         this.channels.add(channel);
+//     }
+
+//     public void showList() {
+//         for (YoutubeChannel channel : this.channels) {
+//             System.out.println(this.name + " has subscribed to " + channel.channelName);
+//         }
+//     }
+// }
+
+interface YoutubeChannel {
+
+    public void uploadVideo(String video);
+
+    public void addSubscriber(String name);
+
+    public void removeSubscriber(String name);
+
+    public void notifySubscribers(String video);
+}
+// using observer design pattern
+/* reduces decoupling, increases scalability and flexibility
+ * readablke, maintainable and more modular code
+ */
+class YoutubeChannelImp implements YoutubeChannel {
+
+    public List<String> subscribers = new ArrayList<>();
+    public List<String> videos = new ArrayList<>();
+
+    @Override
+    public void addSubscriber(String name) {
+        this.subscribers.add(name);
     }
 
-    public void addSubscriber(Subscriber subscriber) {
-        this.subscriberList.add(subscriber);
+    @Override
+    public void removeSubscriber(String name) {
+        this.subscribers.remove(name);
     }
 
+    @Override
     public void uploadVideo(String video) {
-        this.video = video;
-        notifySubscribers(video);
+        this.videos.add(video);
     }
 
+    @Override
     public void notifySubscribers(String video) {
 
-        for (Subscriber subscriber : this.subscriberList) {
-            System.out.println("hello " + subscriber.name + " a new video " + this.video + " has been uploaded");
+        for (String subscriber : this.subscribers) {
+            System.out.println("Hey " + subscriber + " a new video  " + video + " has been uploaed");
         }
     }
 
-}
-
-class Subscriber {
-    public String name;
-    public String id;
-    public String email;
-    public ArrayList<YoutubeChannel> channels = new ArrayList<>();
-
-    Subscriber(String name, String id, String email) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-    }
-
-    public void addChannel(YoutubeChannel channel) {
-        this.channels.add(channel);
-    }
-
-    public void showList() {
-        for (YoutubeChannel channel : this.channels) {
-            System.out.println(this.name + " has subscribed to " + channel.channelName);
-        }
-    }
 }
 
 public class observerDesignPattern {
 
     public static void main(String[] args) {
-        YoutubeChannel channel1 = new YoutubeChannel("Madhav Reacts");
-        Subscriber madhav = new Subscriber("madhav", "123", "madhav.mhjn01@gmail.com");
-        madhav.addChannel(channel1);
-        channel1.addSubscriber(madhav);
-        channel1.uploadVideo("meme compilation");
-        madhav.showList();
+        // YoutubeChannel channel1 = new YoutubeChannel("Madhav Reacts");
+        // Subscriber madhav = new Subscriber("madhav", "123",
+        // "madhav.mhjn01@gmail.com");
+        // madhav.addChannel(channel1);
+        // channel1.addSubscriber(madhav);
+        // channel1.uploadVideo("meme compilation");
+        // madhav.showList();
+        YoutubeChannelImp channel=new YoutubeChannelImp();
+        channel.addSubscriber("alice");
+        channel.addSubscriber("bob");
+        channel.uploadVideo("meme compilation");
+        channel.notifySubscribers("meme compliation");
     }
 }
