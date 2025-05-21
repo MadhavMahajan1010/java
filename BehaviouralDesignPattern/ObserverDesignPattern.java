02dcbcb7f0b0aa4f0764f6e4c9b7473b923482de
Madhav10Mahajan <madhav.mhjn01@gmail.com>
2025-05-22 00:45:58 +0530

    changes

---
+++ BehaviouralDesignPattern/ObserverDesignPattern.java
@@ -0,0 +1,72 @@
+/* it is used when we want to send to send some info/notification among all objects similar to subscribers of a yt channel
+ * so instead of indivdually notifying each subscriber we can communicate to them together as a whole
+ * this reduces coupling and is an eddicient approach since it saves time
+ */
+
+package BehaviouralDesignPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class YoutubeChannel {
+
+    public String channelName;
+    public String video;
+    public ArrayList<Subscriber> subscriberList = new ArrayList<>();
+
+    YoutubeChannel(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public void addSubscriber(Subscriber subscriber) {
+        this.subscriberList.add(subscriber);
+    }
+
+    public void uploadVideo(String video) {
+        this.video = video;
+        notifySubscribers(video);
+    }
+
+    public void notifySubscribers(String video) {
+
+        for (Subscriber subscriber : this.subscriberList) {
+            System.out.println("hello " + subscriber.name + " a new video " + this.video + " has been uploaded");
+        }
+    }
+
+}
+
+class Subscriber {
+    public String name;
+    public String id;
+    public String email;
+    public ArrayList<YoutubeChannel> channels = new ArrayList<>();
+
+    Subscriber(String name, String id, String email) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public void addChannel(YoutubeChannel channel) {
+        this.channels.add(channel);
+    }
+
+    public void showList() {
+        for (YoutubeChannel channel : this.channels) {
+            System.out.println(this.name + " has subscribed to " + channel.channelName);
+        }
+    }
+}
+
+public class ObserverDesignPattern {
+
+    public static void main(String[] args) {
+        YoutubeChannel channel1 = new YoutubeChannel("Madhav Reacts");
+        Subscriber madhav = new Subscriber("madhav", "123", "madhav.mhjn01@gmail.com");
+        madhav.addChannel(channel1);
+        channel1.addSubscriber(madhav);
+        channel1.uploadVideo("meme compilation");
+        madhav.showList();
+    }
+}
---
+++ BehaviouralDesignPattern/strategyDesignPattern.java
@@ -0,0 +1,12 @@
+package BehaviouralDesignPattern;
+
+/* it is used to show how objects communicate with one another after creation in which we used factory design pattern
+ * then comes behavioural desing pattern which means communication between the objects  
+ */
+
+public class strategyDesignPattern {
+    
+    public static void main(String[] args){
+
+    }
+}
