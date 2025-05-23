0e70fb477976e82f069cd07efb72b762958836c6
Madhav10Mahajan <madhav.mhjn01@gmail.com>
2025-05-23 15:49:45 +0530

    command design pattern

--- LLD/BehaviouralDesignPattern/commandDesignPattern.java
+++ LLD/BehaviouralDesignPattern/commandDesignPattern.java
@@ -1,6 +1,64 @@
-/* it is used to wrap all the commands inside an object*/
+/* it is used to wrap all the commands inside an object */
 package BehaviouralDesignPattern;
 
-public class commandDesignPattern {
+class Channel {
+    String name;
+    Integer channelNo;
+
+    Channel(String name, Integer channelNo) {
+        this.name = name;
+        this.channelNo = channelNo;
+    }
+}
+
+class Tv {
+
+    Channel channel;
+    Integer volume;
+    boolean state;
     
+    public void turnOn(){
+        this.state=true;
+    }
+    public void turnOff(){
+        this.state=false;
+    }
+    public void changeChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public void increaseVolume() {
+        volume++;
+    }
+
+    public void decreaseVolume() {
+        volume--;
+    }
+ 
+}
+
+class Remote {
+    Tv tv;
+
+    public void changeChannel(Channel channel) {
+        this.tv.changeChannel(channel);
+    }
+
+    public void increaseVolume() {
+        this.tv.increaseVolume();
+    }
+
+    public void decreaseVolume(){
+        this.tv.decreaseVolume();
+    }
+    public void turnOff(){
+        this.tv.turnOff();
+    }
+    public void turnOn(){
+        this.tv.turnOn();
+    }
+}
+
+public class commandDesignPattern {
+
 }
